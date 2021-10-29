f58e47c52121e55b2bef1f97fa005b21dff0ffa7
Eder Santos <3derms@gmail.com>
2021-10-29 16:23:14 -0300

    Cria o endpoint GET /talker/:id

--- middlewares/validateTalkers.js
+++ middlewares/validateTalkers.js
@@ -1,15 +1,31 @@
 const fs = require('fs').promises;
+
 const HTTP_OK_STATUS = 200;
 const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const TALKER_NOT_FOUND = 'Pessoa palestrante não encontrada';
 
 const getAllTalkers = async (req, res) => {
-    try {
-      const allTalkers = await fs.readFile('./talker.json', 'utf8');
-      if (!allTalkers) return res.status(HTTP_OK_STATUS).json([]);
-      return res.status(HTTP_OK_STATUS).json(JSON.parse(allTalkers));
-    } catch (err) {
-      return res.status(BAD_REQUEST).json(err);
-    }
-  };
+  try {
+    const allTalkers = await fs.readFile('./talker.json', 'utf8');
+    if (!allTalkers) return res.status(HTTP_OK_STATUS).json([]);
+    return res.status(HTTP_OK_STATUS).json(JSON.parse(allTalkers));
+  } catch (err) {
+    return res.status(BAD_REQUEST).json(err);
+  }
+};
 
-module.exports = { getAllTalkers };
+const getTalkerById = async (req, res) => {
+  try {
+    const responseJson = await fs.readFile('./talker.json', 'utf8');
+    const responseArray = JSON.parse(responseJson);
+    const { id } = req.params;
+    const talkerFound = responseArray.find((talker) => talker.id === Number(id));
+    if (!talkerFound) return res.status(NOT_FOUND).json({ message: TALKER_NOT_FOUND });
+    return res.status(HTTP_OK_STATUS).json(talkerFound);
+  } catch (err) {
+    return res.status(BAD_REQUEST).json(err);
+  }
+};
+
+module.exports = { getAllTalkers, getTalkerById };
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const router = express.Router();
-const { getAllTalkers } = require('../middlewares/validateTalkers');
+const { getAllTalkers, getTalkerById } = require('../middlewares/validateTalkers');
 
 router.get('/talker', getAllTalkers);
+router.get('/talker/:id', getTalkerById);
 
 module.exports = router;
