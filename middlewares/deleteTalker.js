425bdfba2fa82086a22f659027d5dca42712fb03
Eder Santos <3derms@gmail.com>
2021-11-03 00:12:21 -0300

    altera nome de variavel para testar avaliador

--- middlewares/deleteTalker.js
+++ middlewares/deleteTalker.js
@@ -4,11 +4,11 @@ const { HTTP_OK_STATUS, TALKER_WAS_DELETED } = require('./messages');
 
 const deleteTalker = async (req, res) => {
     const { id } = req.params;
-    const talkerJsn = await fs.readFile('./talker.json', 'utf-8');
-    const talker = JSON.parse(talkerJsn);
+    const talkerJson = await fs.readFile('./talker.json', 'utf-8');
+    const talker = JSON.parse(talkerJson);
     const response = talker.find((value) => value.id !== Number(id));
-    const responseJsn = JSON.stringify(response);
-    await fs.writeFile('./talker.json', responseJsn);
+    const responseJson = JSON.stringify(response);
+    await fs.writeFile('./talker.json', responseJson);
     res.status(HTTP_OK_STATUS).send({ message: TALKER_WAS_DELETED });
   };
 
